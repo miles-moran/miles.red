2434844c26f396c2887f5d23786a14d143ffc122
Miles <miles@karmacheck.com>
2022-01-28 16:51:41 -0600

    a

--- src/components/me/Bio.jsx
+++ src/components/me/Bio.jsx
@@ -7,8 +7,11 @@ export const Bio = () => {
       <h1 className="home-h1" style={{ fontSize: "4.5em", fontWeight: 500, marginBottom: 0 }}>
         Bio
       </h1>
-      <div style={{ fontWeight: "400" }} target="_blank">
-        Yes, I have a hotmail.
+      <div className="home-p">
+        <div style={{ fontWeight: "400" }} target="_blank">
+          Under Construction
+        </div>
+        <div>Yes, I have hotmail.</div>
       </div>
     </div>
   );
